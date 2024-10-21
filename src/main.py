437b57e1a8bc5afe0aa7b0b34b984dcfffa3a802
LC_Linkous <L.C.Linkous@gmail.com>
2024-10-21 13:32:18 -0400

    added placeholder main

---
+++ src/main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+##-------------------------------------------------------------------------------\
+#   Objective Function Suite
+#   './src/main.py'
+#   placeholder main for the objective function suite
+#   functions can be tested based on the graph generating code in their 
+#       individual directories 
+#
+#   Author(s): Lauren Linkous (LINKOUSLC@vcu.edu)
+#   Last update: October 21, 2024
+##-------------------------------------------------------------------------------\
+
